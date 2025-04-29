8162d69c509d1f04ed5a718f2898d742cfd9966b
erick-mms <erickmota1703@gmail.com>
2025-04-29 19:08:59 -0300

    Update and rename 10_quest_IFRN.c to 11_quest_IFRN.c

--- IFRN/11_quest_IFRN.c
+++ IFRN/11_quest_IFRN.c
@@ -4,9 +4,9 @@ int main(){
     int dias,meses=0,ano=0;
     printf("Ha quantos dias estamos sem acidentes?\n");
     scanf("%d", &dias);
+    ano=dias/360;
+    dias=dias%360;
     meses=dias/30;
-    ano=meses/12;
-    
-    printf("passou-se %d ano(s), %d mes(es) e %d dia(s) sem acidentes\n", ano, meses); 
-    
+    dias=dias%30;
+    printf("passou-se %d ano(s), %d mes(es) e %d dia(s) sem acidentes\n", ano, meses, dias); 
 }
