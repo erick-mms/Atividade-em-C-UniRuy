75722496d6553458e078743a210556792b3b8ff9
erick-mms <erickmota1703@gmail.com>
2025-05-12 01:06:20 -0300

    Update 27_quest_IFSC.c

--- IFSC/27_quest_IFSC.c
+++ IFSC/27_quest_IFSC.c
@@ -1,1 +1,19 @@
-
+#include <stdio.h>
+int main(){
+    int A,B,C,D,E,DigitoV,S;
+    
+    printf("Informe o primeiro digito\n");
+    scanf("%d", &A);
+    printf("Informe o segundo digito\n");
+    scanf("%d", &B);
+    printf("Informe o terceiro digito\n");
+    scanf("%d", &C);
+    printf("Informe o quarto digito\n");
+    scanf("%d", &D);
+    printf("Informe o quinto digito\n");
+    scanf("%d", &E);
+    
+    S=(6*A)+(5*B)+(4*C)+(3*D)+(2*E);
+    DigitoV=S % 7;
+    printf("O digito verificador e: %d\n", DigitoV);
+}
