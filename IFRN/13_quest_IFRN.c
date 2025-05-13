8eb83a779ff006c04c8452f1846a44800e1284ec
erick-mms <erickmota1703@gmail.com>
2025-05-13 16:19:21 -0300

    Update 13_quest_IFRN.c

--- IFRN/13_quest_IFRN.c
+++ IFRN/13_quest_IFRN.c
@@ -4,7 +4,7 @@ int main(){
     
     printf("Insira um Numero de 3 algarismos: \n");
     scanf("%d", &num);
-    cen= (num % 10);  // + ((N % 100) / 10) * 10 + (N / 100); 
+    cen= (num % 10);  
     dez=((num % 100) / 10);
     uni= (num / 100);
     printf("CENTENA = %d \n DEZENA = %d \n UNIDADE = %d", uni,dez,cen);
