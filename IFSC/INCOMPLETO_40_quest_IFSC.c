e0ad2396ac26c245f496f4abac23837ea7cbf7df
erick-mms <erickmota1703@gmail.com>
2025-05-15 13:41:49 -0300

    Create INCOMPLETO_40_quest_IFSC.c

---
+++ IFSC/INCOMPLETO_40_quest_IFSC.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int quant_paraf,quant_porcas,quant_arruelas;
+    float paraf=1,porcas=0.5,arruelas=0.25;
+    char nome_cliente[30];
+
+    
+    printf("Qual seu nome?");
+    fgets(nome_cliente, 20, stdin);
+    printf("Quantos Parafusos foram comprados?");
+    scanf("%d", &quant_paraf);    
+    printf("Quantas porcas foram compradas?");
+    scanf("%d", &quant_porcas);    
+    printf("Quantas Arruelas foram compradas?");
+    scanf("%d", &quant_arruelas);    
+
+    printf
+}
