033bbaba0da749fe338cc06845ad18767102ee6d
erick-mms <erickmota1703@gmail.com>
2025-06-02 00:43:37 -0300

    Create 70_quest_IFSC.c

---
+++ IFSC/70_quest_IFSC.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int par;
+    printf("Insira um numero: ");
+    scanf("%d", &par);
+    if (par%2==0)
+    { printf("Verdadeiro.");}
+    else printf("Falso.");
+}
